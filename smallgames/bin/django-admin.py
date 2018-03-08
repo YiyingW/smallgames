5818fb120cfa70ec727037f95d1d01b6e975ace1
Yiying Wang <wangyydut@gmail.com>
2018-03-07 17:24:36 -0800

    update readme

---
+++ smallgames/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/yiyingwang/Desktop/DirectBuy/project/smallgames/smallgames/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
