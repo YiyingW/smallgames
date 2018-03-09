fc69f0c93787c5b7a9d1463ed44d78b25e70c4ad
Yiying Wang <wangyydut@gmail.com>
2018-03-09 11:13:40 -0800

    add test for index page, add return to home button on game 0 and game 1 pages

--- yiying/games/tests.py
+++ yiying/games/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import resolve
+from django.urls import reverse
+from .views import index, detail
 
 # Create your tests here.
+
+class IndexTests(TestCase):
+
+    def setUp(self):
+        url = reverse('games:index')
+        self.response = self.client.get(url)
+
+    def test_index_view_status_code(self): 
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_index_url_resolves_home_view(self):
+        view = resolve('/')
+        self.assertEquals(view.func, index)
